6a7a48deb4c75c354083102cbb2e942f6d12eadc
Art San <artsan.mcc@gmail.com>
2024-04-10 13:41:23 +0700

    ok

---
+++ backend/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+	await app.listen(3000)
+}
+bootstrap()
+
+// на 1:11:03  начинает деплой
+// https://www.youtube.com/watch?v=8Dc5kqDOzo8
+// import { NestFactory } from '@nestjs/core'
+// import { AppModule } from './app.module'
+// import { PrismaService } from './prisma.service'
+
+// async function bootstrap() {
+// 	const app = await NestFactory.create(AppModule)
+
+// 	app.enableCors()
+// 	const prismaService = app.get(PrismaService)
+// 	await prismaService.enableShutdownHooks(app)
+
+// 	await app.listen(process.env.PORT || 3000)
+// }
+// bootstrap()
