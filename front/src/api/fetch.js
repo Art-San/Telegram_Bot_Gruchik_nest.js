1574c09cd762e08f1ebf2a281953a8ef5b1fef8f
Art San <artsan.mcc@gmail.com>
2024-06-05 17:34:26 +0700

    ok

--- front/src/api/fetch.js
+++
@@ -1,9 +0,0 @@
-import axios from './interceptors'
-
-export const api = async ({ url, method, body }) => {
-  return axios({
-    method,
-    url,
-    data: body
-  })
-}
--- front/src/page/TestPage/TestPage.jsx
+++
@@ -1,9 +0,0 @@
-// export const TestPage = () => {
-//   return <h1 className=" text-3xl font-bold text-white">Hello world worl!</h1>
-// }
-
-const TestPage = () => {
-  return <div className=" text-3xl font-bold text-stone-700">TestPage</div>
-}
-
-export default TestPage
