d961b2e3ab3f893097922d1621712cb289f90e04
Art San <artsan.mcc@gmail.com>
2024-06-16 21:08:21 +0700

    ok

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,33 +1,30 @@
-// import { useEffect } from 'react'
-// import { Routes } from './Layouts/Routes'
-// import { useTelegram } from './hooks/useTelegram'
-// import { useUserContext } from './context/useUser'
-// import { useUser } from './pages/Auth/hooks/useUser'
+import { useEffect } from 'react'
+import { Routes } from './Layouts/Routes'
+import { useTelegram } from './hooks/useTelegram'
+import { useUserContext } from './context/useUser'
+import { useUser } from './pages/Auth/hooks/useUser'
 
-// function App() {
-//   const { telegramId = '721836748' } = useTelegram()
-//   const { data: user, isPending } = useUser(String(telegramId))
-//   const { setCurrentUser } = useUserContext()
+function App() {
+  const { telegramId = '721836748' } = useTelegram()
+  const { data: user, isPending } = useUser(String(telegramId))
+  const { setCurrentUser } = useUserContext()
 
-//   useEffect(() => {
-//     if (user) {
-//       setCurrentUser(user)
-//     }
-//   }, [user, setCurrentUser])
+  useEffect(() => {
+    if (user) {
+      setCurrentUser(user)
+    }
+  }, [user, setCurrentUser])
 
-//   console.log(isPending)
-//   console.log(user)
+  if (isPending) return <div>Loading...</div>
 
-//   if (isPending) return <div>Loading...</div>
+  return (
+    <>
+      <Routes isAuthorized={user?.isAdmin} />
+    </>
+  )
+}
 
-//   return (
-//     <>
-//       <Routes isAuthorized={user?.isAdmin} />
-//     </>
-//   )
-// }
-
-// export default App
+export default App
 
 //===========================================================
 // .. макет от gpt
@@ -59,46 +56,46 @@
 // export default App;
 //==================================================================
 // перввый макет
-import { Routes, Route } from 'react-router-dom'
-import ProductList from './components/ProductList/ProductList'
-import Header from './components/Header/Header'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { useAuthContext } from './context/AuthContext'
-import { Toaster } from 'sonner'
-import Login from './pages/Auth/Login'
-import AddOrderPage from './pages/AddOrderPage/AddOrderPage'
-import OrdersPage from './pages/Orders/OrdersPage/OrdersPage'
-import TestPage from './pages/TestPage/TestPage'
-import AddOrderSitePage from './pages/AddOrderPage/AddOrderSitePage'
-import OrderDetails from './pages/Orders/OrdersPage/OrderDetails'
+// import { Routes, Route } from 'react-router-dom'
+// import ProductList from './components/ProductList/ProductList'
+// import Header from './components/Header/Header'
+// // import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// // import { useAuthContext } from './context/AuthContext'
+// import { Toaster } from 'sonner'
+// import Login from './pages/Auth/Login'
+// import AddOrderPage from './pages/AddOrderPage/AddOrderPage'
+// import OrdersPage from './pages/Orders/OrdersPage/OrdersPage'
+// import TestPage from './pages/TestPage/TestPage'
+// import AddOrderSitePage from './pages/AddOrderPage/AddOrderSitePage'
+// import OrderDetails from './pages/Orders/OrdersPage/OrderDetails'
 
-function App() {
-  return (
-    <>
-      <div className=" flex flex-col h-screen">
-        <header>
-          <Header />
-        </header>
-        <div className="flex flex-col items-center justify-center">
-          <Routes>
-            <Route index element={<ProductList />} />
-            <Route path={'/orders'} element={<OrdersPage />} />
-            <Route path={'/orders/:orderId'} element={<OrderDetails />} />
-            <Route path={'/add_order'} element={<AddOrderPage />} />
-            <Route path={'/add_order_page'} element={<AddOrderSitePage />} />
-            <Route path={'/test'} element={<TestPage />} />
-            <Route path={'/test_2'} element={<AddOrderSitePage />} />
+// function App() {
+//   return (
+//     <>
+//       <div className=" flex flex-col h-screen">
+//         <header>
+//           <Header />
+//         </header>
+//         <div className="flex flex-col items-center justify-center">
+//           <Routes>
+//             <Route index element={<ProductList />} />
+//             <Route path={'/orders'} element={<OrdersPage />} />
+//             <Route path={'/orders/:orderId'} element={<OrderDetails />} />
+//             <Route path={'/add_order'} element={<AddOrderPage />} />
+//             <Route path={'/add_order_page'} element={<AddOrderSitePage />} />
+//             <Route path={'/test'} element={<TestPage />} />
+//             <Route path={'/test_2'} element={<AddOrderSitePage />} />
 
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <footer className="fixed bottom-0 left-0 w-full">
-          {/* <Footer /> */}
-        </footer>
-      </div>
-      <Toaster theme="dark" position="bottom-right" duration={1500} />
-    </>
-  )
-}
+//             <Route path="/login" element={<Login />} />
+//           </Routes>
+//         </div>
+//         <footer className="fixed bottom-0 left-0 w-full">
+//           {/* <Footer /> */}
+//         </footer>
+//       </div>
+//       <Toaster theme="dark" position="bottom-right" duration={1500} />
+//     </>
+//   )
+// }
 
-export default App
+// export default App
--- vite/src/components/Header/PublicHeader.tsx
+++ vite/src/components/Header/PublicHeader.tsx
@@ -1,45 +1,29 @@
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useUserContext } from '@/context/useUser'
 
-const PublicHeader: React.FC = () => {
+const PublicHeader = () => {
   const { currentUser } = useUserContext()
-
+  console.log(12, 'PublicHeader', currentUser?.userAvatar)
   return (
-    <header className="w-screen h-16 bg-white shadow-md flex justify-center items-center gap-2 px-4">
+    <header className="w-screen h-16 bg-white shadow-md flex justify-center items-center gap-4 px-4">
       {/* <header className="w-screen h-16 bg-white shadow-md flex justify-between items-center px-4"> */}
       <div className="flex space-x-4">
         <span>Заказы</span>
         <span>Профиль</span>
-        <span>{currentUser?.userName || 'null'}</span>
       </div>
-      <img
-        src={currentUser?.userAvatar || 'avatar.png'}
-        alt="Аватар пользователя"
-        className="h-10 w-10 rounded-full"
-      />
+      <div className=" flex flex-col item-start">
+        <Avatar>
+          <AvatarImage
+            src={
+              currentUser?.userAvatar ? currentUser.userAvatar : 'avatar.png'
+            }
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <span className=" text-xs">{currentUser?.userName || 'null'}</span>
+      </div>
     </header>
   )
 }
 
 export default PublicHeader
-
-// import React from 'react';
-// import { useUser } from '../UserContext';
-// import { useUserContext } from '@/context/useUser';
-
-// const Header: React.FC = () => {
-//   const { currentUser } = useUser();
-
-//   if (!currentUser) {
-//     return null;
-//   }
-
-//   return (
-//     <header>
-//       <img src={currentUser.userAvatar} alt="User Avatar" />
-//       <span>{currentUser.userName}</span>
-//     </header>
-//   );
-// };
-
-// export default Header;
